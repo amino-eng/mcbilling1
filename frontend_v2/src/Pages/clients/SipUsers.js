156a302b2734756561080977ca415c3cda64d0a8
eyaznouda <trabelsi.eya03@icloud.com>
2025-02-24 11:26:55 +0100

    24/02/2025

--- frontend_v2/src/Pages/clients/SipUsers.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SipUsers = () => {
-  return <h1>SIP Users Page</h1>;
-};
-
-export default SipUsers;
--- frontend_v2/src/Pages/clients/clients.js
+++ frontend_v2/src/Pages/clients/clients.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function clients() {
   return (
-    <div><h1>CLIENT</h1></div>
+    <div><h1>client</h1></div>
   )
 }
